7eae094c00904319ee547dda67cf9cacde0d40b2
AbiMosa <dre7am@hotmail.com>
2019-05-29 14:18:58 +0100

    added rating

--- src/Components/Hotels/Hotel/Hotel.js
+++ src/Components/Hotels/Hotel/Hotel.js
@@ -5,9 +5,23 @@ const Hotel = (props) => {
     // console.log(props);
     return (
         <div className={[classes.Hotel, 'rflex'].join(' ')}>
-            <div className={[classes.HotelImgContainer, 'flexItem'].join(' ')}>
+            <div className={[classes.ImgContainer, 'flexItem'].join(' ')}>
                 <img src={props.establishment.ImageUrl} alt={props.establishment.Name}/>
             </div>
+            <div className={classes.ContentContainer}>
+                <h3>{props.establishment.Name}</h3>
+                <div className={classes.Stars}>{props.establishment.Stars}</div>
+                <p>
+                    <span> {props.establishment.Location} </span>
+                    <span>( {props.establishment.Distance.toFixed(2)} Miles away from center )</span>
+                </p>
+            </div>
+
+            <div className={classes.PriceContainer}>
+                <p> {props.establishment.UserRatingTitle} </p>
+                <p> {props.establishment.UserRatingCount} </p>
+            </div>
+            <div className={classes.UserRating}>{props.establishment.UserRating}</div>
         </div>
     );
 };
